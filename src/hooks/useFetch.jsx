745b3875089459b84043831a0cf97fb00b66d7d1
Roshanshrivas <roshanshrivas11@gmail.com>
2024-06-14 16:21:07 +0530

    MovieAPk

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setLoading("loading....");
+        setData(null);
+        setError(null);
+
+        fetchDataFromApi(url)
+        .then((res) => {
+            setLoading(false);
+            setData(res);
+        })
+        .catch((err) => {
+            setLoading(false);
+            setError("Something when Wrong")
+        });
+    }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
+
+
+
+
+
+
+
+
+
+
+
+
+
